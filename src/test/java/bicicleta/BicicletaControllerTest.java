5a7634999144adcc5b5980d09a761d71f1b13c10
Cecilia <cecilia028@edu.unirio.br>
2022-01-08 01:22:26 -0300

    gitlabci 1a

--- src/test/java/bicicleta/BicicletaControllerTest.java
+++ src/test/java/bicicleta/BicicletaControllerTest.java
@@ -16,7 +16,7 @@ import kong.unirest.Unirest;
 
 class BicicletaControllerTest {
 
-    private static JavalinApp app = new JavalinApp(); 
+    private static JavalinApp app = new JavalinApp(); //testando 
     private String bikeJson = JavalinJson.toJson(ControllerBicicleta.mock.banco);
 
     @BeforeAll
